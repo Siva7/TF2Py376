154ccc24257959cf0dda3d30783d1199b5d826e3
Siva7 <siva.chaitanya0111@gmail.com>
2020-09-14 13:29:15 +0530

    "Includes Basic RNN"

--- Kaggle/1_Titanic/Titanic_Pred.py
+++ Kaggle/1_Titanic/Titanic_Pred.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow import keras as tfk
 from sklearn.model_selection import train_test_split
+import seaborn as sns
 pd.set_option('display.max_columns',None)
 train_file_path = r"Kaggle\1_Titanic\train.csv"
 test_file_path = r'Kaggle\1_Titanic\test.csv'
@@ -27,18 +28,32 @@ out_columns = ["PassengerId","Pclass","Sex_encoded","Age_encoded","SibSp","Parch
 encoded_train_data = encode_data(train_raw_df)[out_columns]
 encoded_test_data = encode_data(test_raw_df)[out_columns[:-1]]
 
-input_layer = tfk.layers.Input(shape=8)
-first_dense_layer = tfk.layers.Dense(20,activation=tfk.activations.relu,kernel_initializer='normal', bias_initializer='zeros')(input_layer)
-second_dense_layer = tfk.layers.Dense(20, activation=tfk.activations.relu, kernel_initializer='normal',
-                                      bias_initializer='zeros')(first_dense_layer)
-for i in range(20):
-    second_dense_layer = tfk.layers.Dense(20,activation=tfk.activations.relu,kernel_initializer='normal', bias_initializer='zeros')(second_dense_layer)
-    second_dense_layer = tfk.layers.Dense(20,activation=tfk.activations.relu,kernel_initializer='normal', bias_initializer='zeros')(first_dense_layer)
+# sns.pairplot(encoded_train_data,hue='Survived')
+
+input_layer = tfk.layers.Input(shape=4)
+first_dense_layer = tfk.layers.Dense(10,activation=tfk.activations.tanh)(input_layer)
+second_dense_layer = tfk.layers.Dense(10, activation=tfk.activations.tanh)(first_dense_layer)
+
+second_dense_layer = tfk.layers.Dense(10, activation=tfk.activations.tanh)(second_dense_layer)
+second_dense_layer = tfk.layers.Dense(10, activation=tfk.activations.tanh)(second_dense_layer)
+second_dense_layer = tfk.layers.Dense(10, activation=tfk.activations.tanh)(second_dense_layer)
 
 second_dense_layer = tfk.layers.Dropout(0.6)(second_dense_layer)
-output = tfk.layers.Dense(2,tfk.activations.softmax,kernel_initializer='normal', bias_initializer='zeros')(second_dense_layer)
 
-model = tfk.Model(inputs=[input_layer],outputs=[output])
+
+linear_input =  tfk.layers.Input(shape=3)
+linear_dense_layer_one = tfk.layers.Dense(10,activation=tfk.activations.linear)(linear_input)
+linear_dense_layer_two = tfk.layers.Dense(10, activation=tfk.activations.linear)(linear_dense_layer_one)
+
+linear_dense_layer_two = tfk.layers.Dense(10, activation=tfk.activations.linear)(linear_dense_layer_two)
+linear_dense_layer_two = tfk.layers.Dense(10, activation=tfk.activations.linear)(linear_dense_layer_two)
+
+linear_drop_out_layer = tfk.layers.Dropout(0.3)(linear_dense_layer_two)
+
+combine_linear_and_non_linear = tfk.layers.concatenate([second_dense_layer,linear_drop_out_layer])
+output = tfk.layers.Dense(2,tfk.activations.softmax,kernel_initializer='normal', bias_initializer='zeros')(combine_linear_and_non_linear)
+
+model = tfk.Model(inputs=[input_layer,linear_input],outputs=[output])
 
 model.summary()
 
@@ -46,11 +61,15 @@ X = encoded_train_data[out_columns[:-1]]
 Y = encoded_train_data[out_columns[-1]]
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.1)
 
+X_train_tuple = (X_train[["Age_encoded","SibSp","Parch","Embarked_encoded"]],X_train[["Pclass","Sex_encoded","Fare_encoded"]])
+X_test_tuple = (X_test[["Age_encoded","SibSp","Parch","Embarked_encoded"]],X_test[["Pclass","Sex_encoded","Fare_encoded"]])
+encoded_test_data_tuple = (encoded_test_data[["Age_encoded","SibSp","Parch","Embarked_encoded"]],encoded_test_data[["Pclass","Sex_encoded","Fare_encoded"]])
+
 
 optimizer = tfk.optimizers.Adam(0.001)
 loss_func = tfk.losses.sparse_categorical_crossentropy
 model.compile(optimizer=optimizer,loss=[loss_func],metrics=["accuracy"])
-training_detail = model.fit(x=X_train, y=Y_train, verbose=1,epochs=500,
+training_detail = model.fit(x=X_train_tuple, y=Y_train, verbose=1,epochs=500,
                             batch_size=20,callbacks=tf.keras.callbacks.EarlyStopping(patience=200,restore_best_weights=True,monitor="accuracy",min_delta=0.01))
 
 
@@ -62,15 +81,15 @@ values = train_res.value_counts()
 accuracy = values[True]/(values[True]+values[False])
 model.summary()
 
-model.evaluate(X_train,Y_train)
-model.evaluate(X_test,Y_test)
+model.evaluate(X_train_tuple,Y_train)
+model.evaluate(X_test_tuple,Y_test)
 
 
 
 training_df=pd.DataFrame(training_detail.history)
 training_df.plot()
 plt.show()
-final_predections = np.argmax(model.predict(encoded_test_data),axis=1)
+final_predections = np.argmax(model.predict(encoded_test_data_tuple),axis=1)
 
 result_df = pd.concat([encoded_test_data["PassengerId"],pd.Series(final_predections)],axis=1)
 result_df.columns = ["PassengerId","Survived"]
---
+++ Tute/2_RNN_Basics/1_Basic_RNN.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow.keras as tfk
+def generate_time_series(batch_size,n_steps):
+    freq1 = np.random.rand(batch_size,1)
+    time = np.linspace(0,1,n_steps)
+    series = 2*np.sin((time-freq1)*10)
+    noise = 0.1*(np.random.rand(batch_size,n_steps)-0.5)
+    print(series.shape)
+    print(noise.shape)
+    res_series = series+noise
+    return  res_series[...,np.newaxis]
+
+n_steps = 50
+total_instances = 10000
+series = generate_time_series(total_instances,n_steps+1)
+
+print("Series Generated of Shape ::"+str(series.shape))
+x_train,y_train = series[:7000,:n_steps],series[:7000,-1]
+x_valid,y_valid = series[7000:9000,:n_steps],series[7000:9000,-1]
+x_test,y_test = series[9000:,:n_steps],series[9000:,-1]
+
+
+print("Training Data shape = "+str(x_train.shape)+":"+str(y_train.shape))
+print("Training Data shape = "+str(x_valid.shape)+":"+str(y_valid.shape))
+print("Training Data shape = "+str(x_test.shape)+":"+str(y_test.shape))
+
+# Linear Model
+
+input = tfk.layers.Input(shape=(50))
+output =  tfk.layers.Dense(1)(input)
+model = tfk.Model(inputs=[input],outputs=[output])
+model.compile(loss = [tfk.losses.MSE],optimizer = tfk.optimizers.SGD(0.01))
+
+x_train_sqzed = np.squeeze(x_train,axis=-1)
+y_train_sqzed = np.squeeze(y_train,axis=-1)
+
+x_valid_sqzed = np.squeeze(x_valid,axis=-1)
+y_valid_sqzed = np.squeeze(y_valid,axis=-1)
+
+x_test_sqzed = np.squeeze(x_test,axis=-1)
+y_test_sqzed = np.squeeze(y_test,axis=-1)
+
+log_dir = r'./logs'
+tensor_flow_call_back = tf.keras.callbacks.TensorBoard(log_dir, histogram_freq=1)
+
+model.fit(x=x_train_sqzed,y=y_train_sqzed,epochs=2,validation_data=(x_valid_sqzed,y_valid_sqzed),
+          callbacks=[
+            tensor_flow_call_back
+])
+
+result=model.evaluate(x_test_sqzed,y_test_sqzed)
+print("Lineage Regression Base Linea Loss  for 2 epochs"+str(result))
+
+# 0.0026
+
+y_pred = model.predict(x_test_sqzed)
+
+plt.plot(y_pred)
+plt.plot(y_test_sqzed)
+plt.show()
+
+input_rnn = tfk.layers.Input(shape=(None,1))
+simple_rnn = tfk.layers.SimpleRNN(1)(input_rnn)
+
+rnn_model = tfk.Model(inputs=[input_rnn],outputs=[simple_rnn])
+rnn_model.compile(optimizer=tfk.optimizers.SGD(0.01),loss=[tfk.losses.MSE])
+
+log_dir = r'./logs'
+tensor_flow_call_back = tf.keras.callbacks.TensorBoard(log_dir, histogram_freq=1)
+
+rnn_model.fit(x=x_train,y=y_train,epochs=2,validation_data=(x_valid,y_valid),
+          callbacks=[
+            tensor_flow_call_back
+])
+
+result=rnn_model.evaluate(x_test_sqzed,y_test_sqzed)
+print("Basic RNN for 2 epochs"+str(result))
+
+y_pred = rnn_model.predict(x_test)
+plt.plot(y_pred)
+plt.plot(y_test)
+plt.show()
+
